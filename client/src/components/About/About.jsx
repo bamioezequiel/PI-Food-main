555073ee92afb5f209ae1fd31be4c7a835cb2dec
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-07-08 18:19:54 -0300

    about modified. finished

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import style from './About.module.css';
+import { TbWorld } from "react-icons/tb";
+import {
+    DiReact,
+    DiJavascript1,
+    DiPostgresql,
+    DiCss3,
+    DiHtml5,
+} from "react-icons/di";
+import {
+    SiRedux,
+    SiSequelize,
+    SiExpress,
+    SiNodedotjs,
+    SiLinkedin,
+    SiGithub,
+} from "react-icons/si";
 
 export default function About() {
 
@@ -17,74 +33,149 @@ export default function About() {
 
     return (
         <div className={style.container}>
+            <button className={style.lenguage} onClick={ (e) => handleClick(e) }>
+                <TbWorld className={style.lenguage_icon} />
+            </button>
             {
                 (lenguage === 'ES')
                 ? <div className={style.container_about}>
-                    <div className={style.container_title}>
-                        <button className={style.lenguage} onClick={ (e) => handleClick(e) }>{lenguage}</button>
+                    <div className={style.container_title}>   
                         <h1 className={style.title}>Food</h1>
                     </div>
                     <p className={style.description}>
-                        Food es una aplicación web de recetas de cocinas. Tiene filtrados por tipos de dietas, ordenamientos por alfabeto y health score. En el boton 'more' de la carta, se podra ver el detalle de dicha receta. Se podra editar o eliminar solo las recetas creadas.
-                        Se puede crear una nueva receta en el apartado 'Create Recipe', adentro se podra apreciar una carta de visualización mientras se llenando los datos de la receta. El boton aparecera solo cuando se llenen los campos requeridos (*).
+                        <b>Single-Page Application</b> se desarrolló como una evaluación individual para el Bootcamp <b>"Soy Henry"</b>.
+                        La idea general es crear una aplicación en la cual se puedan ver distintas recetas de comida junto con información relevante de las mismas utilizando la api externa spoonacular y a partir de ella poder, entre otras cosas: 
+                        <b> Buscar recetas; Filtrarlos / Ordenarlos; Crear, Editar, Eliminar recetas.</b> 
                     </p>
-                    
+
                     <h4>Recetas actuales:</h4>
                     <div className={style.box}>
                         <span className={style.counter}>{countAllRecipes}</span>
                     </div>
 
-                    <h4>Herramientas utilizadas</h4>
-                    <div className={style.tools}>
-                        JavaScript,
-                        React Js,
-                        Redux,
-                        HTML,
-                        CSS,
-                        Node Js,
-                        Express,
-                        PostgreSQL,
+                    <h4>Fue creado con las siguientes tecnologías:</h4>
+                    <div className={style.tech_list}>
+                        <li>
+                        <DiReact className={style.react_icon} />
+                        React
+                        </li>
+                        <li>
+                        <SiRedux className={style.react_icon} /> Redux
+                        </li>
+                        <li>
+                        {" "}
+                        <DiJavascript1 className={style.react_icon} /> Javascript
+                        </li>
+                        <li>
+                        <SiSequelize className={style.react_icon} />
                         Sequelize
+                        </li>
+                        <li>
+                        <DiPostgresql className={style.react_icon} />
+                        PostgreSQL
+                        </li>
+                        <li>
+                        <DiCss3 className={style.react_icon} />
+                        Css
+                        </li>
+                        <li>
+                        <DiHtml5 className={style.react_icon} />
+                        Html
+                        </li>
+                        <li>
+                        <SiExpress className={style.react_icon} />
+                        Express
+                        </li>
+                        <li>
+                        <SiNodedotjs className={style.react_icon} />
+                        Node.js
+                        </li>
                     </div>
-                    <div className={style.social}>
-                        <a href="https://www.linkedin.com/in/ezequielbamio/" target='_black'><img src="https://img.icons8.com/stickers/65/000000/linkedin.png" alt="Linkedin" /></a>
-                        <a href="https://github.com/bamioezequiel/" target='_black'><img src="https://img.icons8.com/stickers/65/000000/github.png" alt="Github" /></a>
+                    <hr/>
+                    
+                    <div className={style.about_pages}>
+                        <a href="https://www.linkedin.com/in/ezequielbamio/"
+                            target="_blank"
+                            rel="noreferrer">
+                            <SiLinkedin className={style.social_links_icons} color="#0A66C2" />
+                        </a>
+                        <a href="https://github.com/bamioezequiel"
+                            target="_blank"
+                            rel="noreferrer">
+                            <SiGithub className={style.social_links_icons} color="black" />
+                        </a>
                     </div>
-                    <small className={style.dev}>Diseñado por Bamio Ezequiel</small>
+                    <small className={style.dev}>Diseñado por Bamio Ezequiel</small> 
                 </div>
                 : <div className={style.container_about}>
-                <div className={style.container_title}>
-                    <button className={style.lenguage} onClick={ (e) => handleClick(e) }>{lenguage}</button>
-                    <h1 className={style.title}>Food</h1>
-                </div>
-                <p className={style.description}>
-                    Food is a web application for cooking recipes. It has filters by types of diets, sorts by alphabet and health score. In the 'more' button of the menu, you can see the details of that recipe. You can edit or delete only the created recipes.
-                    A new recipe can be created in the 'Create Recipe' section, inside you will be able to see a visualization letter while the recipe data is filled. The button will appear only when the required fields are filled in (*).
-                </p>
-                
-                <h4>Current recipes:</h4>
-                <div className={style.box}>
-                    <span className={style.counter}>{countAllRecipes}</span>
-                </div>
+                    <div className={style.container_title}>   
+                        <h1 className={style.title}>Food</h1>
+                    </div>
+                    <p className={style.description}>
+                        <b>Single-Page Application</b> was developed as an individual assessment for the <b>"Soy Henry"</b> Bootcamp.
+                        The general idea is to create an application in which you can see different food recipes along with relevant information about them using the spoonacular external api and from it you can, among other things:
+                        <b> Search recipes; Filter / Sort them; Create, Edit, Delete recipes.</b> 
+                    </p>
 
-                <h4>Used tools</h4>
-                <div className={style.tools}>
-                    JavaScript,
-                    React Js,
-                    Redux,
-                    HTML,
-                    CSS,
-                    Node Js,
-                    Express,
-                    PostgreSQL,
-                    Sequelize
+                    <h4>Current recipes:</h4>
+                    <div className={style.box}>
+                        <span className={style.counter}>{countAllRecipes}</span>
+                    </div>
+
+                    <h4>It was created with the following technologies:</h4>
+                    <div className={style.tech_list}>
+                        <li>
+                        <DiReact className={style.react_icon} />
+                        React
+                        </li>
+                        <li>
+                        <SiRedux className={style.react_icon} /> Redux
+                        </li>
+                        <li>
+                        {" "}
+                        <DiJavascript1 className={style.react_icon} /> Javascript
+                        </li>
+                        <li>
+                        <SiSequelize className={style.react_icon} />
+                        Sequelize
+                        </li>
+                        <li>
+                        <DiPostgresql className={style.react_icon} />
+                        PostgreSQL
+                        </li>
+                        <li>
+                        <DiCss3 className={style.react_icon} />
+                        Css
+                        </li>
+                        <li>
+                        <DiHtml5 className={style.react_icon} />
+                        Html
+                        </li>
+                        <li>
+                        <SiExpress className={style.react_icon} />
+                        Express
+                        </li>
+                        <li>
+                        <SiNodedotjs className={style.react_icon} />
+                        Node.js
+                        </li>
+                    </div>
+                    <hr/>
+                    
+                    <div className={style.about_pages}>
+                        <a href="https://www.linkedin.com/in/ezequielbamio/"
+                            target="_blank"
+                            rel="noreferrer">
+                            <SiLinkedin className={style.social_links_icons} color="#0A66C2" />
+                        </a>
+                        <a href="https://github.com/bamioezequiel"
+                            target="_blank"
+                            rel="noreferrer">
+                            <SiGithub className={style.social_links_icons} color="black" />
+                        </a>
+                    </div>
+                    <small className={style.dev}>Desing by Bamio Ezequiel</small>
                 </div>
-                <div className={style.social}>
-                    <a href="https://www.linkedin.com/in/ezequielbamio/" target='_black'><img src="https://img.icons8.com/stickers/65/000000/linkedin.png" alt="Linkedin" /></a>
-                    <a href="https://github.com/bamioezequiel/" target='_black'><img src="https://img.icons8.com/stickers/65/000000/github.png" alt="Github" /></a>
-                </div>
-                <small className={style.dev}>Desing by Bamio Ezequiel</small>
-            </div>
             }
         </div>    
     )
